debe305f3edd0a6796e1f2e9558f34f8fc998a7e
wisE-web <68653821+wisE-web@users.noreply.github.com>
2022-03-01 19:09:23 -0500

    Add files via upload

---
+++ swappingFile.py
@@ -0,0 +1,13 @@
+def swapFileData ():
+     file1='sample1.txt'
+     file2='sample2.txt'
+
+     with open(file1, 'r') as a:
+         data_a = a.read()
+     with open(file2, 'r') as b:
+         data_b = b.read()
+     with open(file1, 'w') as a:
+         a.write(data_b)
+     with open(file2, 'w') as b:
+         b.write(data_a)
+swapFileData()
